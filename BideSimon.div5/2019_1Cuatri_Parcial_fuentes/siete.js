50948557028da18c669b67c9949a3832dd86a571
simonBide <40763612+simonBide@users.noreply.github.com>
2019-03-09 15:44:55 -0300

    Add files via upload

--- BideSimon.div5/2019_1Cuatri_Parcial_fuentes/siete.js
+++ BideSimon.div5/2019_1Cuatri_Parcial_fuentes/siete.js
@@ -1,20 +1,45 @@
 function mostrar()
 {
-    var altura;
-    var sexo;
+	var sexo;
+	var altura;
+	var acumuladorAltura = 0;
+	var promedioAltura;
+	var alturaMin;
+	var alturaSexoMin;;
+	var contadorMujeresAltas = 0;
 
-    for ( var i = 1 ; i <= 5 ; i++)
-    {
-    altura = parseInt(prompt("Ingrese la altura del jugador."));
-    while( altura < 0 || altura > 250 || isNaN(altura) )
-        {
-        altura = parseInt(prompt("Error. La altura debe estar entre 0 y 250. Reingrese."));
-        }
-    sexo = prompt("Ingrese el sexo del jugador: ").toLowerCase();
-    while( sexo != f && sexo != m )
-    {
-        sexo = prompt("Error. Reingrese sexo ( f o m ). ");
-    }
-    }
+	for ( i = 0 ; i < 5 ; i++)
+	{
+			sexo=prompt("Ingrese el sexo.");
 
+		while ( isNaN(sexo) == false || sexo != "f" && sexo != "m")
+		{
+			sexo = prompt("Error. Reingrese sexo. ( f o m)");
+		}
+			altura=parseInt(prompt("Ingrese la altura"));
+		while ( altura < 0 || altura > 250 || isNaN(altura))
+		{
+			altura=parseInt(prompt("Error. Reingrese la altura"));
+		}
+		
+
+	acumuladorAltura = acumuladorAltura + altura;
+
+		if ( altura < alturaMin || i == 0)
+			 {
+			 	alturaMin = altura;
+			 	alturaSexoMin = sexo;
+			 }
+		if ( altura > 190 && sexo == "f" )
+		{
+			contadorMujeresAltas++;
+		}
+
+	}
+
+	promedio = acumuladorAltura/i;
+
+	alert("El promedio de las alturas es: " + promedio);
+	alert("La persona mas baja mide: " + alturaMin + " y su sexo es: " + sexoMin);
+	alert("La cantidad de mujeres mas altas de 190: " + contadorMujeresAltas);
 }
